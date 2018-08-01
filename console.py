ae8bce0faa5475ea5103a1cbc88d6bc511466707
Max Getman <>
2018-08-01 12:00:23 +0100

    debugging

--- console.py
+++ console.py
@@ -141,6 +141,7 @@ if __name__ == '__main__':
                     voltage = float(a.data['STACK_V'].replace('V', ''))
                     temp = float(a.data['STACK_TEMP'].replace('C', ''))
                     power = float(a.data['OUTPUT_POWER'])
+                    print current, voltage, temp, power
                     c.update(current, power, temp, voltage)
                 #a.displayData()
         except (KeyboardInterrupt, SystemExit):
