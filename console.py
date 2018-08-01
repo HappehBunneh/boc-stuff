37d7880ed3e8660198138309013a5b5cae0fee70
Max Getman <>
2018-08-01 11:22:13 +0100

    forcing float division\

--- console.py
+++ console.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
         try:
             a.currentTime = datetime.now()
             days, seconds = (a.currentTime - a.startTime).days, (a.currentTime - a.startTime).seconds
-            a.time_elapsed = str((days * 24) + (seconds / 3600))
+            a.time_elapsed = str((days * 24) + (float(seconds) / 3600))
             data = a.getRawData()
             if data[0]:
                 a.storeData(data)
