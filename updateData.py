e684af2a530cdb820a3c4184fbc01e7cea88803f
Max Getman <>
2018-08-07 12:17:57 +0100

    database change

--- updateData.py
+++ updateData.py
@@ -11,11 +11,11 @@ class Database():
         self.sql = 'drop table data;'
         self.curs.execute(self.sql)
         self.db.commit()
-        self.sql = 'create table data (current float, power float, temperature float, voltage float)'
+        self.sql = 'create table test (ID int NOT NULL AUTO_INCREMENT, current FLOAT, power FLOAT, temperature FLOAT, voltage FLOAT, PRIMARY KEY(ID));'
         self.curs.execute(self.sql)
         self.db.commit()
 
     def update(self, current, power, temperature, voltage):
-        self.sql = 'insert into data values(' + ','.join([str(i) for i in [current, power, temperature, voltage]]) + ')'
+        self.sql = 'insert into data (current, power, temperature, voltage) values (' + ','.join([str(i) for i in [current, power, temperature, voltage]]) + ')'
         self.curs.execute(self.sql)
         self.db.commit()
