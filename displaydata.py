11cc2f50051a0297ef6bf1689b6b3751890272a4
Max Getman <>
2018-08-20 14:07:22 +0100

    hello

--- displaydata.py
+++ displaydata.py
@@ -27,8 +27,9 @@ while True:
     with open('buffer.txt', 'r') as f:
         bufferData = eval(f.read())
     data, serial = eval(bufferData[0]), bufferData[1]
+    print data, serial
     if data != '':
         print_ = maxprint.Print(data, dataVariables)
         print_._print()
-    print serial
+    #print serial
     time.sleep(1)
