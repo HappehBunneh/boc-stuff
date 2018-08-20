ec68f83f5b22d1355277a0e83d1aafb590f7fedd
Max Getman <>
2018-08-20 14:12:33 +0100

    hello

--- displaydata.py
+++ displaydata.py
@@ -30,6 +30,7 @@ while True:
     if data != '':
         print_ = maxprint.Print(data, dataVariables)
         print print_.data
+        print print_.dataVariables
         print_._print()
     #print serial
     time.sleep(1)
