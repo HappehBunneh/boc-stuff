4bd9079724bb7f83d1f9c65b40cb2d96baf5ad0b
Maximus <disgracedandbetrayed@gmail.com>
2018-12-04 19:56:24 +0000

    console.py readData intialisation change

--- console.py
+++ console.py
@@ -145,7 +145,7 @@ class Console():
         self.startReadingPort()
         self.currentTime = datetime.now()
         self.startTime = self.currentTime
-        self.readline().replace('\r\n', '').replace('\r\r', '').replace('\r', ' ')[:-1]
+        print self.readline().replace('\r\n', '').replace('\r\r', '').replace('\r', ' ')[:-1]
         while True:
             try:
                 data = self.getData()
